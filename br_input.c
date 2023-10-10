ac4652644c10e0947cbbcfaae106c36baf49cc0c
KipruttoMutai <mutaikiprutto@gmail.com>
2023-10-10 06:57:12 -0700

    Input Functions

---
+++ br_input.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+/**
+ * get_input - Read the line of input from the user.
+ * Return: Pointer to a buffer containing the user's input.
+ */
+char *get_input(void)
+{
+	static char *previous_input;
+	char *input = NULL;
+	size_t input_size = 0;
+	ssize_t read_result;
+
+	do {
+		prompt();
+		read_result = getline(&input, &input_size, stdin);
+		if (read_result == -1)
+		{
+			free(input);
+			br_puts("\n");
+			return (NULL);
+		}
+		input[read_result - 1] = '\0';
+	} while (input[0] == '\0' || isspace(input[0]));
+	previous_input = input;
+	return (input);
+}
