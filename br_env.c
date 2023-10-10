a5f5e2a897a5e6817a24c4926ea5f7ae34874156
KipruttoMutai <mutaikiprutto@gmail.com>
2023-10-10 08:39:12 -0700

    Environment Functions

---
+++ br_env.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * br_getenv - Get the value of an environment variable
+ * @name: Name of the environment variable
+ * Return: Value of the environment variable, or NULL if it doesn't exist
+ */
+char *br_getenv(const char *name)
+{
+	char **env;
+	size_t name_len = br_strlen(name);
+
+	for (env = environ; *env != NULL; env++)
+	{
+		if (br_strncmp(*env, name, name_len) == 0 && (*env)[name_len] == '=')
+		{
+			return (&(*env)[name_len + 1]);
+		}
+	}
+
+	return (NULL);
+}
