e6a7664059538ab92bc9f7006461259f396a978e
KipruttoMutai <mutaikiprutto@gmail.com>
2023-10-10 08:08:50 -0700

    String Functions

--- br_str.c
+++ br_str.c
@@ -6,12 +6,51 @@
  */
 int br_strlen(const char *s)
 {
-        int i = 0;
+	int i = 0;
 
-        if (!s)
-                return (0);
+	if (!s)
+		return (0);
 
-        while (*s++)
-                i++;
-        return (i);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * br_strdup - duplicates a string
+ * @str: the string to duplicate
+ * Return: pointer to the duplicated string
+ */
+char *br_strdup(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+
+/**
+ * br_strcmp - Compare two strings.
+ * @s1: The first string to compare.
+ * @s2: The second string to compare.
+ * Return: 0 if @s1 and @s2 are equal
+ */
+int br_strcmp(const char *s1, const char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return ((int) (*s1) - (*s2));
 }
