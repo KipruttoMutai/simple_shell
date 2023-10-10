4d26d01944dc3732d67dce41a834bca28298aa48
KipruttoMutai <mutaikiprutto@gmail.com>
2023-10-10 08:34:23 -0700

    String Functions

--- br_str.c
+++ br_str.c
@@ -54,3 +54,31 @@ int br_strcmp(const char *s1, const char *s2)
 
 	return ((int) (*s1) - (*s2));
 }
+
+/**
+ * br_atoi - Converts a string to an integer.
+ * @str: The string to convert.
+ * Return: The integer value of the string.
+ */
+int br_atoi(const char *str)
+{
+	int i;
+	int sign;
+	unsigned int num;
+
+	i = 0;
+	sign = 1;
+	num = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		else if (str[i] >= '0' && str[i] <= '9')
+			num = (num * 10) + (str[i] - '0');
+		else
+			break;
+		i++;
+	}
+	return (num * sign);
+}
