d493ae533dd8c3d855d480a34b4881e753d33469
KipruttoMutai <mutaikiprutto@gmail.com>
2023-10-11 10:03:38 -0700

    Print Functions

--- br_print.c
+++ br_print.c
@@ -16,3 +16,14 @@ void br_puts(char *str)
 		perror("write");
 	}
 }
+
+/**
+ *br_putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int br_putchar(char c)
+{
+	return (write(1, &c, 1));
+}
