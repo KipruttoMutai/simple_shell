2041485de13c7781a3c6d62f955b81164ba224ef
KipruttoMutai <mutaikiprutto@gmail.com>
2023-10-11 10:24:33 -0700

    String Functions

--- br_strs.c
+++ br_strs.c
@@ -24,3 +24,24 @@ int br_strncmp(const char *s1, const char *s2, size_t n)
 
 	return (0);
 }
+
+/**
+ * br_strcpy - copies a string
+ * @dest: the destination
+ * @src: the source
+ * Return: pointer to destination
+ */
+char *br_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
