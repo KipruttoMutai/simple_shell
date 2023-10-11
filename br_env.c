765058002ff0dd1749e180153399c7d0be3e9bd1
KipruttoMutai <mutaikiprutto@gmail.com>
2023-10-11 09:22:00 -0700

    Environment Functions

--- br_env.c
+++ br_env.c
@@ -20,3 +20,69 @@ char *br_getenv(const char *name)
 
 	return (NULL);
 }
+
+/**
+ * br_setenv - Set the value of an environment variable
+ * @args: Arguments (name and value of the environment variable)
+ * Return: Nothing
+ */
+int br_setenv(char **args)
+{
+	char *name, *value;
+
+	if (args[1] == NULL || args[2] == NULL)
+	{
+		perror("Usage: setenv VARIABLE VALUE\n");
+		return (-1);
+	}
+
+	name = args[1];
+	value = args[2];
+
+	if (setenv(name, value, 1) != 0)
+	{
+		perror("setenv");
+		return (-1);
+	}
+	return (0);
+}
+
+/**
+ * br_unsetenv - Unset an environment variable
+ * @args: Arguments name of the environment variable
+ * Return: Nothing
+ */
+int br_unsetenv(char **args)
+{
+	char *name;
+
+	if (args[1] == NULL)
+	{
+		perror("Usage: unsetenv VARIABLE\n");
+		return (-1);
+	}
+
+	name = args[1];
+
+	if (unsetenv(name) != 0)
+	{
+		perror("unsetenv");
+	}
+	return (0);
+}
+
+/**
+ * br_help - displays help information for built-in commands
+ */
+void br_help(void)
+{
+	br_puts("\nShell Version 1.0.0\n\n");
+	br_puts("Usage: ./hsh\n\n");
+	br_puts("Shell built-in commands:\n\n");
+	br_puts("help\t\tDisplay this help information\n\n");
+	br_puts("cd [dir]\tChange the current working directory\n\n");
+	br_puts("env\t\tDisplay the environment variables\n\n");
+	br_puts("setenv\t\tSet an environment variable\n\n");
+	br_puts("unsetenv\tUnset an environment variable\n\n");
+	br_puts("exit\t\tExit the shell\n\n");
+}
