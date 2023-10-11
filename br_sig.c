6908d9c82224624ffd48b8a8d28efad526e25051
KipruttoMutai <mutaikiprutto@gmail.com>
2023-10-11 10:00:54 -0700

    Signal Functions

---
+++ br_sig.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+
+/**
+ * handle_sigint - Signal handler for SIGINT
+ * @sig: Signal number
+ * Return: Nothing
+ */
+void handle_sigint(int sig)
+{
+	(void) sig;
+	br_putchar('\n');
+	br_prompt();
+}
+
+/**
+ * handle_sigquit - Signal handler for SIGQUIT
+ * @sig: Signal number
+ * Return: Nothing
+ */
+void handle_sigquit(int sig)
+{
+	(void) sig;
+	perror("Quit (core dumped)\n");
+	exit(EXIT_SUCCESS);
+}
+
+/**
+ * handle_sigstp - Signal handler for SIGTSTP
+ * @sig: Signal number
+ * Return: Nothing
+ */
+void handle_sigstp(int sig)
+{
+	(void) sig;
+	br_puts("\n");
+	br_prompt();
+}
