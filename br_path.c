6f5109fbdced0a89b4f6d3390e2c865fcba8c3c4
KipruttoMutai <mutaikiprutto@gmail.com>
2023-10-14 01:56:48 -0700

    Path Functions

--- br_path.c
+++ br_path.c
@@ -47,3 +47,20 @@ char *find_in_path(char *command)
 	free(path);
 	return (full_path);
 }
+/**
+ * free_error - frees alloc'd pointers following system error
+ * @argv: pointer to a pointer to an array of pointers
+ * @arg: pointer to a pointer to an array of characters
+ *
+ * Return: void.
+ */
+void free_error(char **argv, char *arg)
+{
+	int i;
+
+	for (i = 0; argv[i]; i++)
+		free(argv[i]);
+	free(argv);
+	free(arg);
+	exit(EXIT_FAILURE);
+}
